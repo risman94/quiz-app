fa3644a268fd6649e4069f55f26432ca89368b45
risman94 <kamarulrisman1994@gmail.com>
2017-10-05 08:46:44 +0800

    set webpack

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// import Todo from "./Todo";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <p>papap </p>
+      </div>
+    );
+  }
+}
+
+export default Home;
